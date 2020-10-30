ff9440e0a517eff4d6efcf80f1afc40f88f0dff0
Mate Rusz <ruszmate33@gmail.com>
2020-10-30 23:30:30 +0100

    links at index working, entry already renders basic text

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,3 +9,9 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    return render(request, "encyclopedia/entry.html", {
+        "entry":entry,
+    })
